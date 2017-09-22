bae940eb6821bcf4a31a83938d7b57548abfa416
Di <damiand05@wp.pl>
2017-09-22 22:15:46 +0200

    Moving scroll view properly

--- DiUtilities/ScrollViewContentMovable.swift
+++ DiUtilities/ScrollViewContentMovable.swift
@@ -24,10 +24,28 @@ public extension ScrollViewContentMovableProtocol where Self: UIViewController {
     }
     
     public func keyboardWillShow(_ notification: Notification) {
-        
+        moveScrollView(.up, for: keyboardHeight(for: notification))
     }
     
     public func keyboardWillHide(_ notification: Notification) {
-        
+        moveScrollView(.down, for: keyboardHeight(for: notification))
     }
+    
+    func moveScrollView(_ direction: ScrollViewMovingDirection, for offset: CGFloat) {
+        var currentScrollViewFrame = movableScrollView.frame
+        var validOffset = offset
+        if direction == .down { validOffset = -offset }
+        currentScrollViewFrame.size.height -= validOffset
+    }
+    
+    private func keyboardHeight(for notification: Notification) -> CGFloat {
+        guard let userInfo = notification.userInfo, let keyboardFrame = userInfo[UIKeyboardFrameEndUserInfoKey] as? NSValue else { return 0 }
+        let keyboardHeight = keyboardFrame.cgRectValue.height
+        return keyboardHeight
+    }
+}
+
+public enum ScrollViewMovingDirection {
+    case up
+    case down
 }
