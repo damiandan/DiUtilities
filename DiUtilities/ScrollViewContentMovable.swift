926f8a4cc19aba63c503d6622158ca998f8be308
Damian Danowski <ddanowski@9bits.pl>
2017-09-22 20:47:05 +0200

    Added ScrollViewContentMovable protocol

---
+++ DiUtilities/ScrollViewContentMovable.swift
@@ -0,0 +1,13 @@
+//
+//  ScrollViewContentMovable.swift
+//  DiUtilities
+//
+//  Created by Di on 22.09.2017.
+//  Copyright © 2017 Di. All rights reserved.
+//
+
+import Foundation
+
+protocol ScrollViewContentMovableProtocol {
+    func setupScrollViewContentMovableProtocol(with scrollView: UIScrollView)
+}
