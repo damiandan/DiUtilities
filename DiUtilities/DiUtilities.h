4bb05af9903d56d57c7e3b2f9adfba65945621df
Damian Danowski <ddanowski@9bits.pl>
2017-09-22 19:28:32 +0200

    Initial Commit

---
+++ DiUtilities/DiUtilities.h
@@ -0,0 +1,19 @@
+//
+//  DiUtilities.h
+//  DiUtilities
+//
+//  Created by Damian Danowski on 22.09.2017.
+//  Copyright © 2017 damiand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DiUtilities.
+FOUNDATION_EXPORT double DiUtilitiesVersionNumber;
+
+//! Project version string for DiUtilities.
+FOUNDATION_EXPORT const unsigned char DiUtilitiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DiUtilities/PublicHeader.h>
+
+
